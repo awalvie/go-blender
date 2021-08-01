ff3c1d494182f7aab5697343379750ac2923c1a0
awalvie <agrawal.vishesh.178@gmail.com>
2021-08-01 22:12:41 +0530

    FEATURE: Add function to initialize new site

--- init.go
+++ init.go
@@ -1,6 +1,22 @@
 package main
 
+import (
+	"os"
+	"path/filepath"
+)
+
 // blenderInit initialize new project repository in the given path
 func blenderInit(path string) {
-
+	paths := []string{"pages", "build", "templates", "static"}
+	var dirPath string
+	for _, p := range paths {
+		dirPath = filepath.Join(path, p)
+		err := os.MkdirAll(dirPath, 0755)
+		if err != nil {
+			ErrorLogger.Println(err)
+			return
+		}
+	}
+	fp, _ := filepath.Abs(path)
+	InfoLogger.Println("Created Project at ", fp)
 }
