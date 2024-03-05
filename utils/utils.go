e8a0d23ba0e75272afb28537ee9b0adb5c8b7160
awalvie <agrawal.vishesh.178@gmail.com>
2024-03-05 13:01:47 +0100

    utils: remove broken Clean function

--- utils/utils.go
+++ utils/utils.go
@@ -3,21 +3,13 @@ package utils
 import (
 	"os"
 	"path"
-	"path/filepath"
 )
 
 // Cleans a given directory, removing all files and subdirs.
 func Clean(dir string) error {
-	files, err := filepath.Glob(filepath.Join(dir, "*"))
+	err := os.RemoveAll(dir)
 	if err != nil {
 		return err
-	}
-
-	for _, file := range files {
-		err = os.RemoveAll(file)
-		if err != nil {
-			return err
-		}
 	}
 	return nil
 }
