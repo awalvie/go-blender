32865ad5bd856b4fc89f2fe2bad4586169154dfd
awalvie <agrawal.vishesh.178@gmail.com>
2024-06-07 14:09:45 +0200

    cmd(build): Add inital build command
    * Add flags
    * Create the output directory
    
    Signed-off-by: awalvie <agrawal.vishesh.178@gmail.com>

--- cmd/build.go
+++ cmd/build.go
@@ -1,7 +1,8 @@
 package cmd
 
 import (
-	"fmt"
+	"log"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -15,12 +16,46 @@ var buildCmd = &cobra.Command{
 
 func init() {
 	// Setup build flags
-	buildCmd.Flags().StringP("config", "c", "config.yaml", "Build config file")
+	buildCmd.Flags().StringP("path", "p", ".", "Path to site source")
+	buildCmd.Flags().StringP("output", "o", "build", "Path to site output")
+	buildCmd.Flags().BoolP("create", "c", false, "Create output directory if it does not exist")
 
 	// Add build command to root command
 	rootCmd.AddCommand(buildCmd)
 }
 
 func buildRun(cmd *cobra.Command, args []string) {
-	fmt.Println("build called")
+	// Get values from flags
+	path := cmd.Flag("path").Value.String()
+	output := cmd.Flag("output").Value.String()
+	create, err := cmd.Flags().GetBool("create")
+	if err != nil {
+		log.Fatalf("Error getting create flag: %v", err)
+	}
+
+	// Check if the path exists
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		log.Fatalf("Path %s does not exist", path)
+	}
+
+	if create {
+		// Create the output directory
+		err := os.MkdirAll(output, 0755)
+		if err != nil {
+			log.Fatalf("Error creating output directory %s: %v", output, err)
+		}
+		log.Printf("Output directory %s created", output)
+	} else {
+		// Check if the output directory exists
+		if _, err := os.Stat(output); os.IsNotExist(err) {
+			log.Fatalf("Output directory %s does not exist", output)
+		}
+	}
+
+	// Build the site:
+	// 1. Read the markdown files in the index directory
+	// 2. Render them using the templates in the templates directory
+	// 3. Write the rendered HTML files to the output directory
+	// 4. Copy the static files to the output directory
+	log.Printf("Building site from %s to %s", path, output)
 }
