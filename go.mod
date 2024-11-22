7afe60b48a3c492ea08ef598fffde53523ab0924
Vishesh agrawal <agrawal.vishesh.178@gmail.com>
2024-11-22 14:02:36 +0100

    init: clean house
    Here we go again
    
    Signed-off-by: Vishesh agrawal <agrawal.vishesh.178@gmail.com>

--- go.mod
+++ go.mod
@@ -1,10 +1,3 @@
 module github.com/awalvie/go-blender
 
 go 1.21
-
-require (
-	github.com/yuin/goldmark v1.4.0
-	github.com/yuin/goldmark-meta v1.0.0
-)
-
-require gopkg.in/yaml.v2 v2.3.0 // indirect
--- main.go
+++ main.go
@@ -1,47 +1,8 @@
 package main
 
-import (
-	"os"
-
-	"github.com/awalvie/go-blender/cli"
-	"github.com/awalvie/go-blender/logging"
-)
+import "fmt"
 
 // main parses arguments and builds website
 func main() {
-	logging.Init()
-	args := os.Args
-
-	if len(args) < 2 {
-		cli.Help()
-		return
-	}
-
-	switch args[1] {
-	case "init":
-		if len(args) < 3 {
-			logging.ErrorLogger.Fatalln("init: too few arguments")
-		}
-
-		initPath := args[2]
-		err := cli.Init(initPath)
-		if err != nil {
-			logging.ErrorLogger.Fatalln("init: ", err)
-		}
-
-	case "build":
-		if len(args) < 3 {
-			logging.ErrorLogger.Fatalln("build: too few arguments")
-		}
-		buildPath := args[2]
-		err := cli.Build(buildPath)
-		if err != nil {
-			logging.ErrorLogger.Fatalln("build: ", err)
-		}
-
-	default:
-		cli.Help()
-		return
-	}
-
+	fmt.Println("here we go again!")
 }
